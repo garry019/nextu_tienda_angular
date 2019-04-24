ce31c66c0d1224b85364b712ba7790b1f578e424
Gabriel Calderón <garry019@gmail.com>
2019-04-24 17:34:23 -0500

    Inicio del app

---
+++ base_de_datos/bd_usuarios.js
@@ -0,0 +1,19 @@
+[
+    {
+        "_id": "5c9e213db57f49ba7ef2fe03",
+        "email": "test@tienda.com",
+        "password": "12345"
+    },
+    {
+        "_id": "5c9f9e5f6e3fae0d082c7962",
+        "email": "test@nextu.com",
+        "password": "12345",
+        "__v": 0
+    },
+    {
+        "_id": "5c9fef6349a311388c11fd0d",
+        "email": "walter@tienda.com",
+        "password": "12345",
+        "__v": 0
+    }
+]
